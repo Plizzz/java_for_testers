8da3bb808137175b611edef44c16ba7593bb2533
revtov_as <revtov_as@tendertech.ru>
2019-04-08 19:06:06 +0300

    Решение задания №2
    Потренироваться использовать функции, классы, объекты и методы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointRunner.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class PointRunner {
+    public static void main(String[] args) {
+        Point p1 = new Point(-2, 5);
+        Point p2 = new Point(4, -7);
+
+        System.out.println(String.format("Расстояние между точками p1 и p2 = %.2f", Point.distance(p1, p2)));
+    }
+}
