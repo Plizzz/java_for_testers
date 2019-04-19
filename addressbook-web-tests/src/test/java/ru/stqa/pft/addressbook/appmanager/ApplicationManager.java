cbf2be597ff646aad868d48754bab52a63f66fdc
revtov_as <revtov_as@tendertech.ru>
2019-04-19 19:09:14 +0300

    Лекция 3.3. Запуск тестов в разных браузерах Задание №7: Научиться запускать тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,20 +1,35 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private ContactHelper contactHelper;
-    private WebDriver wd;
+     private WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private int browser;
+
+    public ApplicationManager(int browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser == BrowserType.MOZILLA) {
+            wd = new FirefoxDriver();
+        } else if(browser == BrowserType.DEFAULT) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.INTERNET_EXPLORER) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost:99/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,11 +4,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
+import sun.plugin2.util.BrowserType;
 
 public class TestBase {
 
     ContactData firstcase = new ContactData("Michael", "Webber", "89862551445", "webberM@google.com");
-    final ApplicationManager app = new ApplicationManager();
+    final ApplicationManager app = new ApplicationManager(BrowserType.DEFAULT);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
