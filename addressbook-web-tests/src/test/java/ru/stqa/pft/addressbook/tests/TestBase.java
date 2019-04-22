4cfef059363d4aeccee388a1cf2354c62748d05e
revtov_as <revtov_as@tendertech.ru>
2019-04-22 11:45:33 +0300

    Удалён неправильный импорт, и добавлен вместо него корректный

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
-import sun.plugin2.util.BrowserType;
 
 public class TestBase {
 
     ContactData firstcase = new ContactData("Michael", "Webber", "89862551445", "webberM@google.com");
-    final ApplicationManager app = new ApplicationManager(BrowserType.DEFAULT);
+    final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
