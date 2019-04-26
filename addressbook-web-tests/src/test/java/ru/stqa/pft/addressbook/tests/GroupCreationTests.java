0a2140a74255d89e40999e1c519ecaa1043ce191
revtov_as <revtov_as@tendertech.ru>
2019-04-26 16:57:24 +0300

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -11,22 +12,16 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    before.add(group);
-    int max = 0;
-    for (GroupData g :
-            after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
+    if (after.stream().max(Comparator.comparingInt(GroupData::getId)).isPresent()) {
+      group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     }
-    group.setId(max);
     before.add(group);
 
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
