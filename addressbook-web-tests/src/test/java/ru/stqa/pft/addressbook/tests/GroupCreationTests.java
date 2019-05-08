b03f0d35fad2ae62143837219483bf7fb179c591
revtov_as <revtov_as@tendertech.ru>
2019-05-08 14:25:10 +0300

    Лекция 5.4. Fluent-интерфейсы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("test2");
+    GroupData group = new GroupData().withName("test2");
 
     app.group().create(group);
 
