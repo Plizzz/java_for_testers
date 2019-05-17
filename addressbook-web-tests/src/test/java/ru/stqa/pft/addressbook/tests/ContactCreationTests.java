21e8a08c5deb7f964ae2048ff247502b2b31de9b
revtov_as <revtov_as@tendertech.ru>
2019-05-17 12:56:12 +0300

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,46 +1,67 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
-    @Test
-    public void testContactCreationTests() {
-        long leftLimit = 10000000001L;
-        long rightLimit = 99999999999L;
-        long randomNumber = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        StringBuilder xml = new StringBuilder();
+        String line = reader.readLine();
 
+        while (line != null) {
+            xml.append(line);
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xStream.fromXML(String.valueOf(xml));
+        return contacts.stream().map(g -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        StringBuilder json = new StringBuilder();
+        String line = reader.readLine();
+
+        while (line != null) {
+            json.append(line);
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+
+        List<ContactData> contacts = gson.fromJson(String.valueOf(json), new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map(g -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreationTests(ContactData contact) {
         Contacts before = app.contact().all();
 
-        File photo = new File("src/test/resources/ninja.png");
-        ContactData newContact = new ContactData()
-                .withFirstname("Michael")
-                .withLastname("Webber")
-                .withHomePhone(Long.toString(randomNumber))
-                .withMobilePhone("+7(917)952-15-65")
-                .withWorkPhone("8 495 533 12 21")
-                .withEmail("webber.M@google.com")
-                .withEmail2("Mi-Web@yahoo.com")
-                .withEmail3("Webber@yandex.ru")
-                .withGroup("test2")
-                .withAddress("438 DARK SPURT\n" +
-                        "SAN FRANCISCO\n" +
-                        "CA 94528\n" +
-                        "USA")
-                .withPhoto(photo);
-
-        app.contact().createContact(newContact);
+        app.contact().createContact(contact);
         app.goTo().homePage();
 
         Contacts after = app.contact().all();
 
         assertThat(after.size(), equalTo(before.size() + 1));
-        assertThat(after, equalTo(before.withAdded(newContact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
+        assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
     }
 }
