ab045b78e842450309cda9a35ba8090ee28c84bb
revtov_as <revtov_as@tendertech.ru>
2019-05-17 16:04:22 +0300

    Лекция 6.9. Конфигурационные файлы для описания тестовых наборов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -57,6 +57,7 @@ public class ContactCreationTests extends TestBase {
 
     @Test(dataProvider = "validContactsFromJson")
     public void testContactCreationTests(ContactData contact) {
+        app.goTo().homePage();
         Contacts before = app.contact().all();
 
         app.contact().createContact(contact);
