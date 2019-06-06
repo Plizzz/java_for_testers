ade292942e849cbd114623e824b9a5e692671dde
revtov_as <revtov_as@tendertech.ru>
2019-06-06 19:51:38 +0300

    Лекция 9.4. REST: взаимодействие с Bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -1,4 +1,56 @@
 package ru.stqa.pft.rest;
 
+import java.util.Objects;
+
 public class Issue {
+    private int id;
+    private String subject;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    Issue withId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    String getSubject() {
+        return subject;
+    }
+
+    Issue withSubject(String subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    Issue withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Issue issue = (Issue) o;
+
+        if (id != issue.id) return false;
+        if (!Objects.equals(subject, issue.subject)) return false;
+        return Objects.equals(description, issue.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (subject != null ? subject.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
 }
